58b6b508e7d80f605387b666290594a22c416bc3
ZyroNex2004 <rohitkumar99421@gmail.com>
2026-01-15 22:50:26 +0530

    changes

--- Tradify/src/Pages/Dashboard.jsx
+++ Tradify/src/Pages/Dashboard.jsx
@@ -1,10 +1,12 @@
 import MainLayout from "../layouts/MainLayout";
+import Charts from "../components/Charts";
 
 export default function Dashboard() {
   return (
     <MainLayout>
       <h1 className="text-xl font-semibold">Welcome to Tradify 🚀</h1>
       <p className="text-gray-600 mt-2">Your trading dashboard</p>
+      <Charts />
     </MainLayout>
   );
 }
