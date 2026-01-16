9f74fc4b30b8ab1b5ea154a7e1553da9b5858dfd
ZyroNex2004 <rohitkumar99421@gmail.com>
2026-01-16 22:47:02 +0530

    issues fixed

--- Tradify/src/App.jsx
+++ Tradify/src/App.jsx
@@ -1,7 +1,8 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Login from "./pages/Login";
-import Dashboard from "./pages/Dashboard";
+import Login from "./Pages/Login";
+import Dashboard from "./Pages/Dashboard";
 import Portfolio from "./Components/Portfolio";
+
 
 
 function App() {
